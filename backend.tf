34e29f42c6fad8cc5136344d8c64e3f5d342c94d
ShashankSG9 <125717184+ShashankSG9@users.noreply.github.com>
2025-04-05 20:28:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
+    bucket = "mydev-project-terraform-7-batch-27-by-jenkins-pipeline"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
